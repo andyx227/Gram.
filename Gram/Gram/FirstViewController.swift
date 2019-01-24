d876f89950a42aee0e7358afed0e0224156a54fd
Andy <andyxue1@gmail.com>
2019-01-24 12:13:33 -0800

    Initial commit

---
+++ Gram/Gram/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Gram
+//
+//  Created by Andy Xue on 1/24/19.
+//  Copyright © 2019 ECS165A. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Gram/Gram/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Gram
+//
+//  Created by Andy Xue on 1/24/19.
+//  Copyright © 2019 ECS165A. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
