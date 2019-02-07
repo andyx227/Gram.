c021e2f47fc354b94ab7caedb38daba39324f56c
Matthew Ostrowski <mostrowski@ucdavis.edu>
2019-02-06 16:20:19 -0800

    add signup api

--- Gram/Gram/Api.swift
+++ Gram/Gram/Api.swift
@@ -10,11 +10,29 @@ import Foundation
 import FirebaseFirestore
 
 struct Api {
+    static var db = Firestore.firestore()
     struct profileInfo {
         var firstName : String
         var lastName : String
         var email : String
-        var password : String
+    }
+    
+    static func signupUser(user:profileInfo, completion: @escaping ApiCompletion) {
+        let docData: [String:Any] = [
+            "firstName" : user.firstName,
+            "lastName" : user.lastName,
+            "email" : user.email,
+            "profilePhoto" : "",
+            "communities" : []
+        ]
+        
+        db.collection("users").document().setData(docData) { err in
+            if let err = err as? String {
+                completion(nil, err)
+            }
+            
+            completion(["response": "good"], nil)
+        }
     }
     
     typealias ApiCompletion = ((_ response: [String: Any]?, _ error: String?) -> Void)
