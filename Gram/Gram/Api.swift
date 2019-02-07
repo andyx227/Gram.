184c4aae7be9c0a40ad8cc0537e80c96208b33ef
jasc812 <jacobamorris997@gmail.com>
2019-02-07 15:46:06 -0800

    Add function type and comments for search and follow queries

--- Gram/Gram/Api.swift
+++ Gram/Gram/Api.swift
@@ -35,6 +35,28 @@ struct Api {
         }
     }
     
+    /**
+     takes two userIDs, the logged in user and the user to
+     follow. maybe check if already following and if so
+     unfollow the user to follow
+    */
+    static func followUser(follower: String, following: String, completion: @escaping ApiCompletion) {
+        //TODO: potentially store doc ID (that is, our UserID)
+        //for the users queried in the search as well as
+        //the primary user
+    }
+    
+    /**
+     take a search string as arguement, like match equivalent
+     for the search string used
+     Returns a list of user structs? should contain user info
+    */
+    static func searchUsers(name: String, completion: @escaping ApiCompletionList) {
+        //TODO: use array contains function to check if
+        //supplied name is a substring
+    }
+    
+    
     typealias ApiCompletion = ((_ response: [String: Any]?, _ error: String?) -> Void)
     typealias ApiCompletionList = ((_ response: [[String: Any]]?, _ error: String?) -> Void)
 }
