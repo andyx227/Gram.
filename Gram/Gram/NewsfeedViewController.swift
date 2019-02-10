89df95ffb97405aafe52f13e37390cb0c9e89e5d
Andy <andyxue1@gmail.com>
2019-02-10 11:58:55 -0800

    Basic ui implemented for displaying search results when searching people.

--- Gram/Gram/NewsfeedViewController.swift
+++ Gram/Gram/NewsfeedViewController.swift
@@ -17,6 +17,7 @@ class NewsfeedViewController: UIViewController, UITableViewDelegate, UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.dismissKeyboard()
         newsfeedTableView.delegate = self
         newsfeedTableView.dataSource = self
         searchPeopleTableView.delegate = self
@@ -32,7 +33,7 @@ class NewsfeedViewController: UIViewController, UITableViewDelegate, UITableView
         }
         
         Api.searchUsers(name: searchText) { (peopleList, error) in
-            guard let _ = error else {
+            if let _ = error {
                 self.searchPeopleTableView.isHidden = true  // Hide People Search TableView
                 self.newsfeedTableView.isHidden = false  // Show Newsfeed
                 return
@@ -49,17 +50,37 @@ class NewsfeedViewController: UIViewController, UITableViewDelegate, UITableView
             }
         }
     }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        self.searchPeopleTableView.isHidden = true
+        self.newsfeedTableView.isHidden = false
+        self.searchBarPeople.resignFirstResponder()
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if tableView == self.searchPeopleTableView {
-            if people.count == 0 {self.searchPeopleTableView.isHidden = true}
+            if people.count == 0 {
+                self.searchPeopleTableView.isHidden = true
+                self.newsfeedTableView.isHidden = false
+            }
+            return people.count
+        } else if tableView == self.newsfeedTableView {
+            return 0
+        } else {  // Should NEVER reach this case!
+            return 0
         }
-        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
-        cell.textLabel?.text = people[indexPath.row].firstName + " " + people[indexPath.row].lastName
-        return cell
+        if tableView == self.searchPeopleTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath) as! PeopleCell
+            cell.username.text = people[indexPath.row].userName
+            return cell
+        } else {
+            print("newsfeed cell")
+            let cell = tableView.dequeueReusableCell(withIdentifier: "photoCardCell", for: indexPath) as! PhotoCardCell
+            return cell
+        }
     }
 }
---
+++ Gram/Gram/PeopleCell.swift
@@ -0,0 +1,15 @@
+//
+//  PeopleCell.swift
+//  Gram
+//
+//  Created by Andy Xue on 2/10/19.
+//  Copyright © 2019 ECS165A. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PeopleCell: UITableViewCell {
+    @IBOutlet weak var username: UILabel!
+    
+}
